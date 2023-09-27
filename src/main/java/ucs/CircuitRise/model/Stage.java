5956324c41bc10a98eabd0642d92b03da6e251b6
leonelEisinger <leoneleisinger@gmail.com>
2023-09-26 22:11:31 -0300

    Update Stage.java

--- src/main/java/ucs/CircuitRise/model/Stage.java
+++ src/main/java/ucs/CircuitRise/model/Stage.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
